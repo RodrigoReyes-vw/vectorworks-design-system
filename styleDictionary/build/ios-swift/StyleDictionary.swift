6f0ce493ca87a0548851190b2dfa855e96cc86ec
Rodrigo Reyes <rsanchez@vectorworks.net>
2021-11-22 14:02:34 -0500

    tokens test

--- styleDictionary/build/ios-swift/StyleDictionary.swift
+++ styleDictionary/build/ios-swift/StyleDictionary.swift
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 26 Aug 2021 17:47:24 GMT
+// Generated on Mon, 22 Nov 2021 18:57:40 GMT
 
 
 import UIKit
@@ -20,33 +20,39 @@ public class StyleDictionary {
     public static let pink90 = rgba( $pink , 50)
     public static let testCond = [object Object]
     public static let vwBase = [object Object]
-    public static let vwuiColorFeedbackError = $vwui-colors.red
-    public static let vwuiColorFeedbackInfo = $vwui-colors.blue
-    public static let vwuiColorFeedbackSuccess = $vwui-colors.green
-    public static let vwuiColorSurfaceDark = $vwui-colors.black
-    public static let vwuiColorSurfaceMedium = $vwui-colors-neutral.95
-    public static let vwuiColorSurfaceWhite = $vwui-colors.white
-    public static let vwuiColorTextPrimaryOnDark = $vwui-colors.white
-    public static let vwuiColorTextPrimaryOnLight = $vwui-colors-neutral.80
-    public static let vwuiColorsBlack = #000000
-    public static let vwuiColorsBlackOpacity75 = rgba($vwui-colors.black , 0.75)
-    public static let vwuiColorsBlackOpacity90 = rgba($vwui-colors.black , 0.9)
-    public static let vwuiColorsBlue = #1B8DDE
-    public static let vwuiColorsGreen = #ffCE7C
-    public static let vwuiColorsNeutral20 = #323434
-    public static let vwuiColorsNeutral35 = #575A5B
-    public static let vwuiColorsNeutral50 = #7D8182
-    public static let vwuiColorsNeutral65 = #A4A7A8
-    public static let vwuiColorsNeutral80 = #CBCDCD
-    public static let vwuiColorsNeutral95 = #F2F2F3
-    public static let vwuiColorsOrange = #FF8200
-    public static let vwuiColorsRed = #DC3545
-    public static let vwuiColorsTeal = #00bcb4
-    public static let vwuiColorsTealOpacity75 = rgba($vwui-colors.teal , .75)
-    public static let vwuiColorsTealOpacity90 = rgba($vwui-colors.teal , .90)
-    public static let vwuiColorsWhite = #FFFFFF
-    public static let vwuiColorsWhiteOpacity75 = rgba($vwui-colors.white , 0.75)
-    public static let vwuiColorsWhiteOpacity90 = rgba($vwui-colors.white , .9)
+    public static let vwuiColorBackgroundAccent1 = $vwui-color.teal
+    public static let vwuiColorBackgroundDark = $vwui-color.black
+    public static let vwuiColorBackgroundLight = $vwui-color.white
+    public static let vwuiColorBackgroundMedium = $vwui-color-neutral.20
+    public static let vwuiColorBlack = #000000
+    public static let vwuiColorBlue = #3B8DDE
+    public static let vwuiColorFeedbackError = #DC3545
+    public static let vwuiColorFeedbackInfo = $vwui-color.blue
+    public static let vwuiColorFeedbackSuccess = $vwui-color-green
+    public static let vwuiColorGreen = #ffCE7C
+    public static let vwuiColorHeadingOnDark = $vwui-color.white
+    public static let vwuiColorHeadingOnLight = $vwui-color.black
+    public static let vwuiColorMagenta = #EE44BC
+    public static let vwuiColorNeutral20 = #F2F2F3
+    public static let vwuiColorNeutral35 = #CBCDCD
+    public static let vwuiColorNeutral50 = #A4A7A8
+    public static let vwuiColorNeutral65 = #7D8182
+    public static let vwuiColorNeutral80 = #575A5B
+    public static let vwuiColorNeutral95 = #323434
+    public static let vwuiColorOrange = #FF8200
+    public static let vwuiColorRed = #DC3545
+    public static let vwuiColorSurfaceDark = $vwui-color.black
+    public static let vwuiColorSurfaceMedium = $vwui-color-neutral.95
+    public static let vwuiColorSurfaceWhite = $vwui-color.white
+    public static let vwuiColorTeal = #00bcb4
+    public static let vwuiColorTextPrimary = $vwui-color.black
+    public static let vwuiColorTextPrimaryOnDark = $vwui-color.white
+    public static let vwuiColorTextSecondary = $vwui-color-neutral.80
+    public static let vwuiColorTextSecondaryOnDark = $vwui-color-neutral.35
+    public static let vwuiColorWhite = #FFFFFF
+    public static let vwuiColorYellow = #FDCD43
+    public static let vwuiColorssTextPrimaryOnDark = $vwui-color.white
+    public static let vwuiColorssTextPrimaryOnLight = $vwui-color-neutral.80
     public static let vwuiFontSize0062 = $vwui-font-size-1000 * .625
     public static let vwuiFontSize0081 = $vwui-font-size-1000 * .8125
     public static let vwuiFontSize0875 = $vwui-font-size-1000 * .8875
