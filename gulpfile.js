b010c675e567c460ac26fa65e26e443e41806c17
Rodrigo Reyes <rsanchez@vectorworks.net>
2021-11-29 16:19:21 -0500

    updated style dictionary with the right format

--- gulpfile.js
+++ gulpfile.js
@@ -6,20 +6,20 @@ const autoprefixer = require("autoprefixer");
 const sourcemaps = require("gulp-sourcemaps");
 
 function css() {
-    return src("src/scss/**/*.{scss,sass}")
-        .pipe(
-            sass({
-                includePaths: ["./node_modules"],
-                fiber: Fiber,
-            }).on("error", sass.logError)
-        )
-        .pipe(postcss([autoprefixer()]))
-        .pipe(sourcemaps.write())
-        .pipe(dest("dist/css"));
+  return src("./assets/scss/**/*.{scss,sass}")
+    .pipe(
+      sass({
+        includePaths: ["./node_modules"],
+        fiber: Fiber,
+      }).on("error", sass.logError)
+    )
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write())
+    .pipe(dest("dist/css"));
 }
 
 function watcher(cb) {
-    watch("src/scss/**/*.{scss,sass}", css, cb);
+  watch("assets/scss/**/*.{scss,sass}", css, cb);
 }
 
 exports.css = css;
